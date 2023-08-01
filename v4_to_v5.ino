adea71194e65e5e16cae442939760026c7bfc616
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2023-08-01 22:55:35 +0500

    Develop (#14)
    * bofore implementing v5 changes
    
    * files changes and path is decided
    
    * almost finalize the termination of messages but not done yet just 1 function need time (prayer Time)
    
    * auto sms execution is added but still not test completely
    
    * time correction for auto sms execution
    
    * error free code and almost all functionality is test but not all of it
    
    * all messages are still not forwarding, only the last one is reciving
    
    * incoming new messages are not well deal in flow
    
    * incomming message bug fixed
    
    * ultra sound sensor is now working fine
    
    * ultra sound reading linked with sms but not tested because of network down
    
    * Develop (#8)
    
    * bofore implementing v5 changes
    
    * files changes and path is decided
    
    * almost finalize the termination of messages but not done yet just 1 function need time (prayer Time)
    
    * auto sms execution is added but still not test completely
    
    * time correction for auto sms execution
    
    * error free code and almost all functionality is test but not all of it
    
    * all messages are still not forwarding, only the last one is reciving
    
    * incoming new messages are not well deal in flow
    
    * incomming message bug fixed
    
    * ultra sound sensor is now working fine
    
    * ultra sound reading linked with sms but not tested because of network down
    
    * unstable code, debugging
    
    * before removing lcd part
    
    * rebooting issue fixed by adjesting thingspeak initializing method
    
    * AT commands are not working
    
    * AT commands not responding issue fixed

--- v4_to_v5.ino
+++ v4_to_v5.ino
@@ -1,6 +1,6 @@
-//$ last work 1/August/23 [01:15 AM]
-// # version 5.0.8
-// rebooting Issue fixed
+//$ last work 1/August/23 [09:59 PM]
+// # version 5.0.9
+// looking for AT commands not working
 
 //`===================================
 #include <DHT.h>
@@ -107,8 +107,8 @@ int currentTargetIndex = 0;
 //`...............................
 
 // #-------------------------------
-#define echoPin 14 //  attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 27 // attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 2  //  attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 15 // attach pin D3 Arduino to pin Trig of HC-SR04
 
 long duration;    // variable for the duration of sound wave travel
 int distance = 0; // variable for the distance measurement
@@ -117,6 +117,8 @@ bool DEBUGGING = true;
 //` in seconds if user enable debugging then it will disable after x seconds
 
 //! * # # # # # # # # # # # # * !
+void say(String str);
+
 void setup() {
   SerialMon.begin(115200);
   // Keep power when running from battery
@@ -156,7 +158,7 @@ void setup() {
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
+  display.setCursor(0, 0);
   // Display static text
   display.println("Hello, world!" + String(random(99)));
   display.display();
@@ -332,6 +334,7 @@ void call(String number) {
   say("ATD+ " + number + ";");
   updateSerial();
 }
+
 void sendSms(String sms) {
 
   if (modem.sendSMS(MOBILE_No, sms)) {
@@ -399,6 +402,7 @@ void getLastMessageAndIndex(String response, String &lastMessage,
 }
 
 String getResponse() {
+  Println("entering getResponse");
   String response = "";
   unsigned int entrySec = millis() / 1000;
   int timeoutSec = 3;
@@ -420,7 +424,6 @@ String getResponse() {
       deleteMessage(newMessageNumber);
     else {
       sendSms("<Unable to execute sms no. {" + String(newMessageNumber) +
-
               "} message : > [ " +
               temp_str.substring(0, temp_str.indexOf(" <not executed>")) +
               " ] from : " + getNumberOfMessage(newMessageNumber));
