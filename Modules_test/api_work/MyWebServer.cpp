40b933b123fd7ceb3fe9495e14ad4362ba450019
M Hamid Jamil <55445517+mhamidjamil@users.noreply.github.com>
2024-09-06 01:00:22 +0500

    Code formated by vs code and added clear method to clear previous input

--- Modules_test/api_work/MyWebServer.cpp
+++ Modules_test/api_work/MyWebServer.cpp
@@ -4,71 +4,75 @@
 MyWebServer::MyWebServer(int port) : server(port), lastReceivedData("") {}
 
 void MyWebServer::begin() {
-    server.on("/", HTTP_GET, std::bind(&MyWebServer::handleRoot, this));
-    server.on("/data", HTTP_GET, std::bind(&MyWebServer::handleGetRequest, this));
-    server.on("/data", HTTP_POST, std::bind(&MyWebServer::handlePostRequest, this));
-    server.onNotFound(std::bind(&MyWebServer::handleNotFound, this));
-    server.begin();
+  server.on("/", HTTP_GET, std::bind(&MyWebServer::handleRoot, this));
+  server.on("/data", HTTP_GET, std::bind(&MyWebServer::handleGetRequest, this));
+  server.on("/data", HTTP_POST,
+            std::bind(&MyWebServer::handlePostRequest, this));
+  server.onNotFound(std::bind(&MyWebServer::handleNotFound, this));
+  server.begin();
 }
 
-void MyWebServer::handleClient() {
-    server.handleClient();
-}
+void MyWebServer::handleClient() { server.handleClient(); }
 
 void MyWebServer::handleRoot() {
-    server.send(200, "text/plain", "Welcome to the ESP32 Web Server");
+  server.send(200, "text/plain", "Welcome to the ESP32 Web Server");
+}
+
+void MyWebServer::clearLastReceivedData() {
+  lastReceivedData = ""; // Clear the stored data
 }
 
 void MyWebServer::handleGetRequest() {
-    String message = "Received GET request!";
-    server.send(200, "application/json", "{\"message\":\"" + message + "\"}");
-    sendDataToPythonServer(message);
+  String message = "Received GET request!";
+  server.send(200, "application/json", "{\"message\":\"" + message + "\"}");
+  sendDataToPythonServer(message);
 }
 
 void MyWebServer::handlePostRequest() {
-    if (server.hasArg("plain")) {
-        lastReceivedData = server.arg("plain");  // Store the received data
-        server.send(200, "application/json", "{\"received\":\"" + lastReceivedData + "\"}");
-        sendDataToPythonServer(lastReceivedData);
-    } else {
-        server.send(400, "application/json", "{\"error\":\"No message body received\"}");
-    }
+  if (server.hasArg("plain")) {
+    lastReceivedData = server.arg("plain"); // Store the received data
+    server.send(200, "application/json",
+                "{\"received\":\"" + lastReceivedData + "\"}");
+    sendDataToPythonServer(lastReceivedData);
+  } else {
+    server.send(400, "application/json",
+                "{\"error\":\"No message body received\"}");
+  }
 }
 
 void MyWebServer::sendDataToPythonServer(const String &data) {
-    if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        http.begin("http://192.168.1.238:6678/esp32");  // Server URL
-        http.addHeader("Content-Type", "application/json");  // Specify the content type
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("http://192.168.1.238:6678/esp32"); // Server URL
+    http.addHeader("Content-Type",
+                   "application/json"); // Specify the content type
 
-        // Create a JSON document
-        StaticJsonDocument<200> jsonDoc;
-        jsonDoc["data"] = data;
+    // Create a JSON document
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["data"] = data;
 
-        // Serialize JSON document to string
-        String requestBody;
-        serializeJson(jsonDoc, requestBody);
+    // Serialize JSON document to string
+    String requestBody;
+    serializeJson(jsonDoc, requestBody);
 
-        // Send POST request
-        int httpResponseCode = http.POST(requestBody);
+    // Send POST request
+    int httpResponseCode = http.POST(requestBody);
 
-        if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println("Response from server: " + response);
-        } else {
-            Serial.println("Error on sending POST: " + String(httpResponseCode));
-        }
-
-        http.end();  // Close connection
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Response from server: " + response);
     } else {
-        Serial.println("WiFi not connected");
+      Serial.println("Error on sending POST: " + String(httpResponseCode));
     }
+
+    http.end(); // Close connection
+  } else {
+    Serial.println("WiFi not connected");
+  }
 }
 
 void MyWebServer::handleNotFound() {
-    server.send(404, "text/plain", "404: Not Found");
+  server.send(404, "text/plain", "404: Not Found");
 }
 
-String MyWebServer::getLastReceivedData() {
-    return lastReceivedData;
-}
+String MyWebServer::getLastReceivedData() { return lastReceivedData; }
--- Modules_test/api_work/api_work.ino
+++ Modules_test/api_work/api_work.ino
@@ -1,45 +1,46 @@
 #include "MyWebServer.h"
-#include <WiFi.h>
 #include "arduino_secrets.h"
+#include <WiFi.h>
 
-const char* ssid = MY_SSID;
-const char* password = MY_PASSWORD;
+const char *ssid = MY_SSID;
+const char *password = MY_PASSWORD;
 
 MyWebServer myServer(80);
 
 void setup() {
-    Serial.begin(115200);
-    WiFi.begin(ssid, password);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-    Serial.println("Connected to WiFi");
-    Serial.println("IP Address: " + WiFi.localIP().toString());
+  Serial.println("Connected to WiFi");
+  Serial.println("IP Address: " + WiFi.localIP().toString());
 
-    myServer.begin();
+  myServer.begin();
 }
 
 void loop() {
-    myServer.handleClient();
+  myServer.handleClient();
 
-    // Print the last received data from the POST request
-    String receivedData = myServer.getLastReceivedData();
-    if (receivedData.length() > 0) {
-        Serial.println("Received data from Python server: " + receivedData);
-    }
+  // Print the last received data from the POST request
+  String receivedData = myServer.getLastReceivedData();
+  if (receivedData.length() > 0) {
+    Serial.println("Received data from Python server: " + receivedData);
+    myServer.clearLastReceivedData();
+  }
 
-    // Example: Send data to Python server
-    // String dataToSend = "Hello from ESP32";
+  // Example: Send data to Python server
+  // String dataToSend = "Hello from ESP32";
 
-      if (Serial.available()) {
+  if (Serial.available()) {
     String dataToSend = Serial.readString();
-    Serial.println("i get: "+dataToSend);
+    Serial.println("i get: " + dataToSend);
     myServer.sendDataToPythonServer(dataToSend);
   }
 
-    // Add a delay to avoid spamming the server
-    // delay(5000);  // Send data every 5 seconds
+  // Add a delay to avoid spamming the server
+  // delay(5000);  // Send data every 5 seconds
 }
